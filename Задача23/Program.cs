c7811df1c72123f6b6f2d1c1962d1feeab0fe4fc
abotnari80 <abotnari80@gmail.com>
2022-03-08 16:31:05 +0300

    Задача 23

---
+++ Задача23/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int cubeNum = 1;
+int cube = 0;
+
+while (cubeNum < number)
+{
+    cube = cubeNum * cubeNum * cubeNum;
+    Console.Write(cube);
+    Console.Write(", ");
+    cubeNum = cubeNum + 1;
+}
+
+cube = cubeNum * cubeNum * cubeNum;
+Console.WriteLine(cube);
