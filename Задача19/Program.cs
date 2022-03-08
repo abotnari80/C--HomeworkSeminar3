7f4474baf3bcac8bd7eb5fb20155984a1301809d
abotnari80 <abotnari80@gmail.com>
2022-03-08 15:51:44 +0300

    Задача 19

---
+++ Задача19/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите 5-ти значное число:");
+int value = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[3];
+int i = 0;
+
+while (i < 2)
+{
+    array[i] = value % 10;
+    value = value / 10;
+    i = i + 1;
+}
+
+array[2] = value % 10;
+int halfValue = (array[0] * 100) + (array[1] * 10) + array[2];
+
+if (value == halfValue)
+{
+    Console.WriteLine("Да");
+}
+
+else
+{
+    Console.WriteLine("Нет");
+}
